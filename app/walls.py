f40a1cc0fda784ded4d1dd2f71519ff6239b2bc7
Jon Taylor <jondtaylor@gmail.com>
2016-02-20 13:39:06 -0800

    wip

--- app/walls.py
+++ app/walls.py
@@ -5,7 +5,10 @@ class Walls():
     def wallCollision(self, game_json):
     	#if game_json == "":
     	#	game_json = "{u'height': 10, u'snakes': [{u'age': 0, u'name': u'', u'health': 100, u'message': u'', u'status': u'alive', u'id': u'72ad0c75-244b-4e30-9169-4584cf4fee28', u'taunt': u'', u'coords': [[2, 2], [2, 2], [2, 2]], u'kills': 0}], u'width': 10, u'turn': 0, u'game': 1, u'food': [], u'mode': u'classic'}";
+    	print game_json
+
     	#game_state = json.loads(game_json)
+    	#print "input: " + str(game_state)
     	#head_x = 0
     	#head_y = 0
     	#board_width = 0
