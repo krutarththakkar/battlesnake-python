ac55bfca0be994fcb330d5b982cf827241a87b6e
BGnoinski <ben.gnoinski@redbrickmedia.com>
2016-02-20 14:11:24 -0800

    Initial start of the module

--- app/gold.py
+++ app/gold.py
@@ -1,6 +1,44 @@
 import json
+import utility
 
-class gold():
-	def __init__(self):
+def distanceToGold():
+    '''
+    get gold position
+    get all snakes head postions
+    Calculate closet to gold 
+    are we closed to gold?
+    if yes check and see if anything in the way. 
 
-    def distanceToGold:
+    '''
+    with open('snake.json', 'r') as f:
+        snakesinfo = json.load(f)
+        # print snakesinfo
+        # print json.dumps(snakes, indent=4)
+
+        our_snakeid = '72ad0c75-244b-4e30-9169-4584cf4fee28'
+        other_snakes = {}
+        our_snake = {}
+        for i in snakesinfo['snakes']:
+            if i['id'] != our_snakeid:
+                snake = i['id']
+                # other_snakes[i]['id'] = {}
+                other_snakes[snake] = {}
+                other_snakes[snake]['coords'] = i['coords'][0]
+                # other_snakes[i]['id'] = 'blah'
+                # other_snakes[i] = []
+            else:
+                snake = i['id']
+                our_snake[snake] = i['coords'][0]
+
+        # for i in other_snakes:
+        #     utility.distanceBetweenTwoPoints(snakesinfo[gold], i
+        print json.dumps(other_snakes, indent=4)
+        print json.dumps(our_snake, indent=4)
+
+        # gold_position = snakeinfo['gold'] #can be cleaned up after by calling this directly in the next functions
+        # our_distance = utility.distance()
+        # our_distance = utility.distance()
+        # our_distance = utility.distance()
+        # our_distance = utility.distance()
+
+distanceToGold()
