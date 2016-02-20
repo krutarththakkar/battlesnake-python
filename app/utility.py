986372c9c88e6f121d19007a7373ba99f8784e01
Evan Hildebrandt <evan.hildebrandt@redbrickmedia.com>
2016-02-20 14:56:34 -0800

    req

--- app/utility.py
+++ app/utility.py
@@ -1,7 +1,6 @@
 import json
 import math
 import random
-from main import boardTypes
 
 def distanceBetweenTwoPoints(point1, point2):
     return  (abs((point2[0] - point1[0])) + abs((point2[1] - point1[1])))
