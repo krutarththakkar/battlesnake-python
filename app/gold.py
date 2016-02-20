277fa8b9115168c75fd4e7bfa54136e33a355a8c
BGnoinski <ben.gnoinski@redbrickmedia.com>
2016-02-20 14:30:02 -0800

    Calculate distance between all snakes and the gold, return if we want to go for the gold

--- app/gold.py
+++ app/gold.py
@@ -12,33 +12,37 @@ def distanceToGold():
     '''
     with open('snake.json', 'r') as f:
         snakesinfo = json.load(f)
+        print snakesinfo
         # print snakesinfo
         # print json.dumps(snakes, indent=4)
 
         our_snakeid = '72ad0c75-244b-4e30-9169-4584cf4fee28'
         other_snakes = {}
         our_snake = {}
+        dist = [] # Distance for other snakes
         for i in snakesinfo['snakes']:
             if i['id'] != our_snakeid:
-                snake = i['id']
-                # other_snakes[i]['id'] = {}
-                other_snakes[snake] = {}
-                other_snakes[snake]['coords'] = i['coords'][0]
-                # other_snakes[i]['id'] = 'blah'
-                # other_snakes[i] = []
+                # print i['coords'][0]
+                dist.append(utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold']))
+
             else:
+                our_dist = utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold'])
                 snake = i['id']
                 our_snake[snake] = i['coords'][0]
 
         # for i in other_snakes:
-        #     utility.distanceBetweenTwoPoints(snakesinfo[gold], i
-        print json.dumps(other_snakes, indent=4)
-        print json.dumps(our_snake, indent=4)
+            # utility.distanceBetweenTwoPoints(snakesinfo[gold], i)
+        print dist
+        print our_dist
 
-        # gold_position = snakeinfo['gold'] #can be cleaned up after by calling this directly in the next functions
-        # our_distance = utility.distance()
-        # our_distance = utility.distance()
-        # our_distance = utility.distance()
-        # our_distance = utility.distance()
+        go_for_the_gold = True
+
+        for i in dist:
+            if our_dist < i: 
+                print 'yay our distance is less go for the gold'
+            else:
+                print 'damn it don\'t go for the gold'
+                go_for_the_gold = False
+
 
 distanceToGold()
