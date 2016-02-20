085e9c9cd38c3d751a487aad39b1b9bd2cb94ecd
BGnoinski <ben.gnoinski@redbrickmedia.com>
2016-02-20 15:39:11 -0800

    Return a direction either north east south or west

--- app/gold.py
+++ app/gold.py
@@ -1,48 +1,46 @@
 import json
 import utility
+our_snakeid = '72ad0c75-244b-4e30-9169-4584cf4fee28'
 
-def distanceToGold():
-    '''
-    get gold position
-    get all snakes head postions
-    Calculate closet to gold 
-    are we closed to gold?
-    if yes check and see if anything in the way. 
+def distanceToGold(snakesinfo):
+    directions = {'north': 0,
+                    'east': 0,
+                    'south': 0,
+                    'west': 0
+                }
 
-    '''
-    with open('snake.json', 'r') as f:
-        snakesinfo = json.load(f)
-        print snakesinfo
-        # print snakesinfo
-        # print json.dumps(snakes, indent=4)
+    dist = [] # Distance for other snakes
+    for i in snakesinfo['snakes']:
+        if i['id'] != our_snakeid:
+            dist.append(utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold']))
 
-        our_snakeid = '72ad0c75-244b-4e30-9169-4584cf4fee28'
-        other_snakes = {}
-        our_snake = {}
-        dist = [] # Distance for other snakes
-        for i in snakesinfo['snakes']:
-            if i['id'] != our_snakeid:
-                # print i['coords'][0]
-                dist.append(utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold']))
+        else:
+            our_dist = utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold'])
+            our_coords = i['coords'][0]
+    print dist
+    print our_dist
 
-            else:
-                our_dist = utility.distanceBetweenTwoPoints(i['coords'][0], snakesinfo['gold'])
-                snake = i['id']
-                our_snake[snake] = i['coords'][0]
+    go_for_the_gold = True
 
-        # for i in other_snakes:
-            # utility.distanceBetweenTwoPoints(snakesinfo[gold], i)
-        print dist
-        print our_dist
+    for i in dist:
+        if our_dist < i: 
+            print 'yay our distance is less go for the gold'
+            print snakesinfo['gold'][0]
+            print our_coords[0]
+            east_west = our_coords[0] - snakesinfo['gold'][0]
+            north_south = our_coords[1] - snakesinfo['gold'][1] 
+            print 'east_west %s north_south%s' % (east_west, north_south)
+            if east_west > 0 and directions['west'] < 100:
+                directions['west'] += 100
+            elif east_west < 0 and directions['east'] < 100:
+                directions['east'] += 100 
 
-        go_for_the_gold = True
-
-        for i in dist:
-            if our_dist < i: 
-                print 'yay our distance is less go for the gold'
-            else:
-                print 'damn it don\'t go for the gold'
-                go_for_the_gold = False
-
-
-distanceToGold()
+            if north_south > 0  and directions['north'] < 100:
+                directions['north'] += 100
+            elif north_south < 0  and directions['south'] < 100:
+                directions['south'] += 100 
+        else:
+            print 'damn it don\'t go for the gold'
+            return {0,0,0,0}
+        if go_for_the_gold is True:
+            return{directions['north'], directions['east'], directions['south'], directions['west']}
