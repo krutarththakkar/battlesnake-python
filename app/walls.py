6e3a949df067049f7db2af70f6d2d30b8e074d77
Jon Taylor <jondtaylor@gmail.com>
2016-02-20 16:14:38 -0800

    fix

--- app/walls.py
+++ app/walls.py
@@ -37,7 +37,6 @@ class Walls():
     		direction.north = 0		
 
     	if snake_head[1] == height - 1: # head on bottom, dont go south
-
     		south_result = 0	
     		direction.south = 0
 
@@ -73,11 +72,11 @@ class Walls():
 		    		direction.east = 0
 		    		print "Snake on right"
 
-		    	if mySnake.head[0] == point_coord[0] and mySnake.head[1] + 1 == point_coord[1]: # snake on top
+		    	if mySnake.head[0] == point_coord[0] and mySnake.head[1] - 1 == point_coord[1]: # snake on top
 					direction.north = 0	
 					print "Snake above"	
 
-		    	if mySnake.head[0] == point_coord[0] and mySnake.head[1] - 1 == point_coord[1]: # head on bottom side, don't go north	
+		    	if mySnake.head[0] == point_coord[0] and mySnake.head[1] + 1 == point_coord[1]: # head on bottom side, don't go north	
 					direction.south = 0
 					print "Snake below"
 
